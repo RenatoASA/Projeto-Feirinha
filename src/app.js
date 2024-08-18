7a18f019af8dec3cd638f043b8060fc888d23019
Renato <renato_asa@hotmail.com>
2024-08-18 16:46:14 -0400

    Final commit

--- src/app.js
+++ src/app.js
@@ -1,9 +1,10 @@
-import express from "express";  
+import express,{json} from "express";  
 
 
 const app = express();
+app.use(json());
 
-const lista =[
+const items =[
     {
         id: 1, 
         name: "Maçã",
@@ -18,16 +19,37 @@ const lista =[
     }
     ];
 
-app.get("/lista", (req, res)=> {
-    res.send(lista);
+app.get("/items", (req, res)=> {
+
+    const item = req.query;
+    
+    if(item.type){
+        const typeList = items.filter(itemList =>{
+            return itemList.type.includes(item.type);
+        });
+
+        return res.send(typeList);
+    }
+
+    res.send(items);
 });
 
-app.get("/lista/:id", (req, res)=>{
+app.get("/items/:id", (req, res)=>{
     const id = req.params.id;
-    const item = lista.find(item=>{
-        return item.id === Number(id);
 
-    })
+    
+
+    if (!Number.isInteger(Number(id)) || Number(id) <= 0) {
+        return res.status(400).send("Bad Request");
+    }   
+
+    const item = items.find(item=>{
+        return item.id === Number(id);
+    });
+
+    if(!item){
+        return res.status(404).send("Not Found");
+    } 
     res.send(item);
 });
 
@@ -35,6 +57,21 @@ app.listen(5000,()=>{
     console.log("o servidor esta rodando na porta 5000");
 });
 
+app.post("/items", (req, res)=>{
+    const listItem = req.body;
+    const repeatedName = items.find((item) => item.name === listItem.name);
+    if(repeatedName){
+        return res.status(409).send("Conflict");
+    }
+    if(!listItem.name || !listItem.quantity || !listItem.type){
+        return res.status(422).send("Unprocessable Entity");
+    }
 
+    items.push({
+        id: items.length + 1,
+        ...listItem
+    });
+    res.status(201).send("Created");
+})
 
 
